36f76b2b1e17f6b65bf7ef0eec18d8e08237bf3d
Damianen <113175544+Damianen@users.noreply.github.com>
2023-12-15 17:14:14 +0100

    add functionality for fxml page loading

--- codecademy/src/main/java/com/example/javafx/GUI.java
+++ codecademy/src/main/java/com/example/javafx/GUI.java
@@ -1,20 +1,22 @@
 package com.example.javafx;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 public class GUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("xml/startPage.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/javafx/fxml/Start.fxml"));
         Parent root = loader.load();
+        
+        primaryStage.setTitle("New Scene");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();   
+    }
 
-        GUIController controller = loader.getController();
-
-        primaryStage.setTitle("Codecademy");
-        primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
+    public void startGui(String args[]) {
+        launch(args);
     }
 }
